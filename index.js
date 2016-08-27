ebe20557ca0f972f7f0ac4aba5c05a7e86e49406
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-08-27 13:25:53 +0600

    working on setup , added error reporting class

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-var program  = require('commander');
+const program  = require('commander');
+
 
 program
    .version('0.1.0')
@@ -7,29 +8,87 @@ program
 
 program
    .command('setup')
-   .description('Setup files for writing migration');
+   .description('Setup files for writing migration')
+   .action(function() {
+         
+         console.log();
+
+   });
+
+
 program
-   .command('create')
-   .description('Creates a json file');
+   .command('make:migration [option]')
+   .description('Creates a json file')
+   .action(function(option) {
+         
+         console.log(option);
+
+   });
 
 program
    .command('migrate')
-   .description('Read schemas from json and then creates json');
+   .description('Read schemas from json and then creates json')
+   .action(function() {
+         
 
-program
-   .command('migrate:undo')
-   .description('creates a json file');
+
+   });
+
 
 program
    .command('migrate:refresh')
-   .description('creates a json file');
+   .description('Deletes current migration and reload it again')
+   .action(function() {
+         
 
+   });
+
+program
+   .command('migration:rollback')
+   .description('To rollback the latest migration operation, you may use the rollback command. This command rolls back the last "batch" of migrations, which may include multiple migration files')
+   .action(function() {
+         
+         console.log();
+
+   });
+program
+   .command('migration:reset')
+   .description('The migrate:reset command will roll back all of your application\'s migrations')
+   .action(function() {
+         
+         console.log();
+
+   });
+
+/*
 
 program
    .arguments('<cmd>')
    .action(function(cmd) {
-   
-//       lib.get(cmd);
+      
+      switch (cmd) {
+
+         case 'setup':
+            break;
+         case 'create':
+            break;
+         case 'make:migration':
+            break;
+         case 'migrate':
+            break;
+         case 'migration:refresh':
+            break;
+         case 'migration:rollback':
+            break;
+         case 'migration:rest':
+            break;
+         default:
+            console.log('wrong command');
+            break;
+
+      }      
+
    });
+ */
 
 program.parse(process.argv);
